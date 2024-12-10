03eee6c1d62050f77a7bddb71c7dce25d01cf7cf
ve3 <kawewong@gmail.com>
2024-12-10 11:15:23 +0700

    Update form vertical spacing.

--- form.php
+++ form.php
@@ -1,5 +1,9 @@
 <form method="post" action="login.php">
-    <input type="email" name="email" value="" placeholder="Email"> (You may use admin@localhost or test@localhost)<br>
-    <input type="password" name="password" value="" placeholder="Password"> (default password id pass)<br>
+    <p>
+        <input type="email" name="email" value="" placeholder="Email"> (You may use admin@localhost or test@localhost)
+    </p>
+    <p>
+        <input type="password" name="password" value="" placeholder="Password"> (default password id pass)
+    </p>
     <button type="submit">Login</button>
 </form>
